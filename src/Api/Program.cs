d7f02abb80d2bc31a0892ac5248cdd55f47ebdca
rgonzalesm-RCorp <55471856+rgonzalesm-RCorp@users.noreply.github.com>
2025-11-28 15:20:34 -0400

    ignore

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -8,7 +8,10 @@ using ApiGuardian.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.WebHost.UseUrls("http://0.0.0.0:5000");
+if (builder.Environment.IsProduction())
+{
+    builder.WebHost.UseUrls("http://0.0.0.0:5000");
+}
 
 builder.Services.AddCors(options =>
 {
--- src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f91aeeb2cf5e0597c2e8228729d7e554ccef8a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f83f95f6c98c31fa59a2e4adc4bc390309cada")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
