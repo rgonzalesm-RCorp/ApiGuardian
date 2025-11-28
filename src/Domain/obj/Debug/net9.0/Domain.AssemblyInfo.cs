5eb8d2fbcf199721f656ea97b3240d2e8f457b67
rgonzalesm-RCorp <55471856+rgonzalesm-RCorp@users.noreply.github.com>
2025-11-28 15:21:42 -0400

    ignore

--- src/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f83f95f6c98c31fa59a2e4adc4bc390309cada")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f02abb80d2bc31a0892ac5248cdd55f47ebdca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
